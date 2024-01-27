8e82d7ec4daf4d4afaf278da5284f86a2b4781df
Tabish <tabish786qidwai@gmail.com>
2024-01-28 04:49:58 +0530

    training model

---
+++ model.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import joblib
+import plotly.express as px
+import streamlit as st
+
+df = pd.read_csv("C:/Users/tabis/Desktop/Laptop price prediction/dataset/Laptop_price.csv")
+
+page = ['Dataset', 'Predict Section']
+
+st.sidebar('Dataset'):
+    st.head()
